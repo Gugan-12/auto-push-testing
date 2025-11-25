3db190f856598ced7580794a9d73b188e8309027
Gugan-12 <guganjai2004@gmail.com>
2025-11-25 11:13:51 +0530

    updated comments

--- function_app.py
+++ function_app.py
@@ -1,6 +1,22 @@
 import azure.functions as func
 import logging
 
+
+
+# On the top-right, click “Add file” → “Upload files”.
+
+# Open your local backend folder on your computer.
+
+# Select all the files and subfolders inside it (not the folder itself).
+
+# Drag and drop them into the GitHub upload area.
+
+# GitHub will show all the files to be uploaded or replaced.
+
+# Existing files (like app.py, requirements.txt, etc.) will be overwritten automatically.
+
+# Scroll down and in the Commit message, type:
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="autopushtesting")
@@ -16,6 +32,19 @@ def autopushtesting(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
+# On the top-right, click “Add file” → “Upload files”.
+
+# Open your local backend folder on your computer.
+
+# Select all the files and subfolders inside it (not the folder itself).
+
+# Drag and drop them into the GitHub upload area.
+
+# GitHub will show all the files to be uploaded or replaced.
+
+# Existing files (like app.py, requirements.txt, etc.) will be overwritten automatically.
+
+# Scroll down and in the Commit message, type:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
